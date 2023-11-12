0fc9cb43f5bf370d3ac2d762052eeccc893cd076
Francisco Grande <fjgrandv@gmail.com>
2023-11-12 01:19:28 +0100

    Add UsersPage

---
+++ src/pages/UsersPage/UsersPage.tsx
@@ -0,0 +1,11 @@
+import UsersPageStyled from "./UsersPageStyled";
+
+const UsersPage = (): React.ReactElement => {
+  return (
+    <UsersPageStyled>
+      <h2 className="page-title">Users</h2>
+    </UsersPageStyled>
+  );
+};
+
+export default UsersPage;
