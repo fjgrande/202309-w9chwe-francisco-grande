3ff526cbf88cb50163832cdc97dede45bf00fed8
Francisco Grande <fjgrandv@gmail.com>
2023-11-12 03:57:40 +0100

    Add Navigation component en App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,17 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import AppStyled from "./AppStyled";
 import UsersPage from "../../pages/UsersPage/UsersPage";
+import Navigation from "../Navigation/Navigation";
 
 const App = (): React.ReactElement => {
   return (
     <AppStyled>
       <h1>DoggyFace</h1>
+      <Navigation />
       <Routes>
         <Route path="/" element={<Navigate to="/users" />} />
         <Route path="/users" element={<UsersPage />} />
+        <Route path="/create" />
       </Routes>
     </AppStyled>
   );
