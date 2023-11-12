e60755cb49624fe15cc113b729ba4ecc8012b704
Francisco Grande <fjgrandv@gmail.com>
2023-11-12 15:48:43 +0100

    Add msw configurations

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,1 +1,21 @@
 import "@testing-library/jest-dom";
+import { beforeAll, afterEach, afterAll } from "vitest";
+import { server } from "./mocks/node";
+import { UsersStructure } from "./store/feature/users/types";
+import MockData from "./mocks/mockData";
+
+export let mockUsers: UsersStructure[] = MockData;
+
+beforeAll(() => {
+  mockUsers = MockData;
+  return server.listen();
+});
+
+afterEach(() => {
+  mockUsers = MockData;
+  return server.resetHandlers();
+});
+
+afterAll(() => {
+  server.close();
+});
