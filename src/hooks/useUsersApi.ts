11f8017b8669aa88546a4d005b68a63e251e7036
Francisco Grande <fjgrandv@gmail.com>
2023-11-12 15:48:09 +0100

    Add useUsersApi

---
+++ src/hooks/useUsersApi.ts
@@ -0,0 +1,19 @@
+import { useCallback } from "react";
+import { UsersStructure } from "../store/feature/users/types";
+import axios from "axios";
+
+const useUsersApi = () => {
+  const apiUrl = import.meta.env.VITE_API_URL;
+
+  const getUsers = useCallback(async () => {
+    const { data: users } = await axios.get<{ users: UsersStructure[] }>(
+      `${apiUrl}/users`,
+    );
+
+    return users;
+  }, [apiUrl]);
+
+  return { getUsers };
+};
+
+export default useUsersApi;
