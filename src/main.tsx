2e463a0ce8aafa00b13fea3ca2fdf73d4e30d1c3
Francisco Grande <fjgrandv@gmail.com>
2023-11-12 20:29:46 +0100

    Add provider store in main

--- src/main.tsx
+++ src/main.tsx
@@ -7,14 +7,18 @@ import App from "./components/App/App";
 import GlobalStyle from "./styles/GlobalStyle";
 import mainTheme from "./styles/mainTheme";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ThemeProvider theme={mainTheme}>
-        <GlobalStyle />
-        <App />
-      </ThemeProvider>
+      <Provider store={store}>
+        <ThemeProvider theme={mainTheme}>
+          <GlobalStyle />
+          <App />
+        </ThemeProvider>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
 );
