064ea0dda970ecc69c96da423d9ded6b793479f0
Francisco Grande <fjgrandv@gmail.com>
2023-11-13 02:11:14 +0100

    Fix useUserAPi sintaxis axios

--- src/hooks/useUsersApi.ts
+++ src/hooks/useUsersApi.ts
@@ -3,14 +3,12 @@ import { UsersStructure } from "../store/feature/users/types";
 import axios from "axios";
 
 const useUsersApi = () => {
-  const apiUrl = import.meta.env.VITE_API_URL;
+  axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 
   const getUsers = useCallback(async () => {
-    const { data: users } = await axios.get<{ users: UsersStructure[] }>(
-      `${apiUrl}/users`,
-    );
+    const { data: users } = await axios.get<UsersStructure[]>("/users");
     return users;
-  }, [apiUrl]);
+  }, []);
 
   return { getUsers };
 };
