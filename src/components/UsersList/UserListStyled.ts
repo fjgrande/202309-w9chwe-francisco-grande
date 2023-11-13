d300db9a44e59ff074dd0de88e90e88cb270c63a
Francisco Grande <fjgrandv@gmail.com>
2023-11-13 01:53:32 +0100

    Add UserList style

---
+++ src/components/UsersList/UserListStyled.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const UserListStyled = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 45px;
+`;
+
+export default UserListStyled;
